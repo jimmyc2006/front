514b502389f7ff0792a2322be095b510ab81d583
shuwei <shuwei@iqianjin.com>
2018-07-02 09:50:19 +0800

    init

---
+++ jsst/src/main/java/front/study/jsst/App.java
@@ -0,0 +1,13 @@
+package front.study.jsst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
